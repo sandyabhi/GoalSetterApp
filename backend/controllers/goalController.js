17cfe755e865134e4d89bf880729f93c8b735f19
Sandyabhi <abhisandy47@gmail.com>
2022-06-02 22:04:10 +0530

    Goal controller and routes

---
+++ backend/controllers/goalController.js
@@ -0,0 +1,29 @@
+const asyncHandler = require("express-async-handler");
+
+const getGoals = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Get goals" });
+});
+
+const setGoal = asyncHandler(async (req, res) => {
+  if (!req.body.text) {
+    //res.status(400).json({ message: "Please add a text" });
+    res.status(400);
+    throw new Error("Please add a text");
+  }
+  res.status(200).json({ message: "Set goals" });
+});
+
+const updateGoal = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: `Update goal ${req.params.id}` });
+});
+
+const deleteGoal = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: `Delete goal ${req.params.id}` });
+});
+
+module.exports = {
+  getGoals,
+  setGoal,
+  updateGoal,
+  deleteGoal,
+};
