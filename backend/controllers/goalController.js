5876060a19d64516986993adc585f6a06e39b676
Sandyabhi <abhisandy47@gmail.com>
2022-06-04 21:35:55 +0530

    Added frontend UI

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,9 +1,10 @@
 const asyncHandler = require("express-async-handler");
 
-const Goal = require("../model/goalModel");
+const Goal = require("../models/goalModel");
+const User = require("../models/userModel");
 
 const getGoals = asyncHandler(async (req, res) => {
-  const goals = await Goal.find();
+  const goals = await Goal.find({ user: req.user.id });
   res.status(200).json(goals);
 });
 
@@ -13,17 +14,33 @@ const setGoal = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("Please add a text");
   }
-  const goals = await Goal.create({
+  const goal = await Goal.create({
     text: req.body.text,
+    user: req.user.id,
   });
-  res.status(200).json(goals);
+  res.status(200).json(goal);
 });
 
 const updateGoal = asyncHandler(async (req, res) => {
   const goal = await Goal.findById(req.params.id);
+
   if (!goal) {
     res.status(400);
     throw new Error("Goal not found");
+  }
+
+  const user = await User.findById(req.user.id);
+
+  // Check for user
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  // Make sure the logged in user matches the goal user
+  if (goal.user.toString() !== user.id) {
+    res.status(401);
+    throw new Error("User not authorized");
   }
 
   const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
@@ -39,6 +56,21 @@ const deleteGoal = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("Goal not found");
   }
+  
+  const user = await User.findById(req.user.id);
+
+  // Check for user
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  // Make sure the logged in user matches the goal user
+  if (goal.user.toString() !== user.id) {
+    res.status(401);
+    throw new Error("User not authorized");
+  }
+
   await goal.remove();
 
   res.status(200).json({ id: req.params.id });
